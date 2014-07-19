802826bc0c400a3b82bac383c0892d168128dfa3
killmous <wcaldwell.email@gmail.com>
2014-07-19 17:54:48 -0500

    PHP Level: Godlike  Changes to be committed: 	modified:   js/main.js 	modified:   php/main.php 	modified:   php/pages/profile.php

--- php/pages/profile.php
+++ php/pages/profile.php
@@ -1,48 +1,61 @@
 <?php
 
 function get_profile() {
+    $name = Util::getUser($_SESSION['bvid'])['name'];
     $page =
 <<<EOD
 <script>
-    $('#hours-submit').on('click', function(e) {
-        e.preventDefault();
-        if(/[0-9]{8}/.test($('#bvid').val())) {
+    $('#profile').on('click', function(e) {
+        $.ajax({
+            type: 'post',
+            url: 'php/main.php',
+            data: {
+                page: "profile"
+            }
+        }).done(function(data) {
+            $('#main').html(data);
+        });
+    });
+    $('#sign_out').on('click', function(e) {
+        $.ajax({
+            type: 'post',
+            url: 'php/main.php',
+            data: {
+                util: "sign_out"
+            }
+        }).done(function(data) {
             $.ajax({
                 type: 'post',
                 url: 'php/main.php',
                 data: {
-                    util: "sign_in",
-                    attr: {
-                        bvid: $('#bvid').val()
-                    }
+                    page: "home"
                 }
             }).done(function(data) {
-                if(data == "success") {
-                    $.ajax({
-                        type: 'post',
-                        url: 'php/main.php',
-                        data: {
-                            page: "profile"
-                        }
-                    }).done(function(data) {
-                        $('#main').html(data);
-                    });
-                }
+                $('#main').html(data);
             });
-        } else {
-            alert('Invalid ID');
-        }
-    });
+        });
+    })
 </script>
-<div class="jumbotron">
-    <h2><img width="50px" src="img/nhs.png"> BVSW NHS <small>Making the world a more honorable place</small></h2>
-</div>
-<div id="hour-form" class="input-group input-group-lg">
-    <span class="input-group-addon">You are currently logged in</span>
-    <input id="bvid" class="form-control" autofocus>
-    <span class="input-group-btn">
-        <button id="hours-submit" class="btn btn-default" type="button">Check</button>
-    </span>
+<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">$name</a>
+    </div>
+    <div class="collapse navbar-collapse">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#" id="profile">Profile</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li class="active"><a href="#" id="sign_out">Logout</a></li>
+      </ul>
+    </div><!--/.nav-collapse -->
+  </div>
 </div>
 EOD;
     echo $page;
