f1479dd7c1e47bd187dfc9dd3b14ada4e2ab1895
Jacob Hegna <jacobhegna@gmail.com>
2014-07-26 20:48:03 -0400

    small changes

--- js/main.js
+++ js/main.js
@@ -16,6 +16,9 @@ $(document).ready(function() {
     }).done(function(data) {
         if(data != 'refresh') {
             $('#main').html(data);
+            if(p == 'home') {
+                $('#home').parent().addClass('active');
+            }
         } else {
 
         }
