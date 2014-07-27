4a539f2ffde781d3eb8b9164dd894929aea0c8b4
killmous <wcaldwell.email@gmail.com>
2014-07-27 00:16:20 -0500

    Almost there...  Changes to be committed: 	modified:   index.html 	new file:   js/bootstrap.min.js 	modified:   php/pages/profile.php

--- php/pages/profile.php
+++ php/pages/profile.php
@@ -5,21 +5,22 @@ function get_profile() {
 <div class="container">
     <h1 style="text-align: center">' . Util::getUser($_SESSION['bvid'])['hours'] . ' Hours Logged</h1><br />
     <div>
-        <ul class="nav nav-tabs" role="tablist">
-            <li class="active"><a href="#">Current Events</a></li>
-            <li><a href="#">Past Events</a></li>
+        <ul id="myTab" class="nav nav-tabs">
+            <li class="active"><a href="#current" data-toggle="tab">Current Events</a></li>
+            <li><a href="#past" data-toggle="tab">Past Events</a></li>
         </ul>
-        <table class="table table-hover" style="margin-top: 50px; text-align: left; font-size: medium;">
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Hours</th>
-                    <th>Date</th>
-                </tr>
-            </thead>
-            <tbody>
+        <div id="myTabContent" class="tab-content">
+            <div class="tab-pane fade active in" id="current">
+                <table class="table table-hover" style="margin-top: 50px; text-align: left; font-size: medium;">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Hours</th>
+                            <th>Date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
 ';
-    # Some fancy code here to do database queries
     $myevents = json_decode($database->get('members', 'events', ['bvid' => $_SESSION['bvid']]));
     foreach ($myevents as $i) {
         $events = $database->get('events', '*', ['id' => $i]);
@@ -31,10 +32,14 @@ function get_profile() {
                 </tr>
 ';
     }
-    # End fancy code
     $page .= '
-            </tbody>
-        </table>
+                    </tbody>
+                </table>
+            </div>
+            <div class="tab-pane fade" id="past">
+                <p>Nothing has happened yet!</p>
+            </div>
+        </div>
     </div>
 </div>
 ';
