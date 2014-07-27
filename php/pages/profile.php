8630889b16f2b327330cd74d7e882a00e5902986
killmous <wcaldwell.email@gmail.com>
2014-07-26 23:15:09 -0500

    Added functionality to display events chosen on profile page.  Changes to be committed: 	modified:   php/pages/profile.php

--- php/pages/profile.php
+++ php/pages/profile.php
@@ -3,16 +3,13 @@ function get_profile() {
     global $database;
     $page = '
 <div class="container">
-    <h1 style="text-align: center">' . Util::getUser($_SESSION['bvid'])['hours'] . ' Hours Logged</h1>
+    <h1 style="text-align: center">' . Util::getUser($_SESSION['bvid'])['hours'] . ' Hours Logged</h1><br />
     <div>
         <ul class="nav nav-tabs" role="tablist">
             <li class="active"><a href="#">Current Events</a></li>
             <li><a href="#">Past Events</a></li>
         </ul>
         <table class="table table-hover" style="margin-top: 50px; text-align: left; font-size: medium;">
-';
-    # Some fancy code here to do database queries
-    $page .= '
             <thead>
                 <tr>
                     <th>Name</th>
@@ -21,23 +18,26 @@ function get_profile() {
                 </tr>
             </thead>
             <tbody>
+';
+    # Some fancy code here to do database queries
+    $myevents = json_decode($database->get('members', 'events', ['bvid' => $_SESSION['bvid']]));
+    foreach ($myevents as $i) {
+        $events = $database->get('events', '*', ['id' => $i]);
+        $page .= '
                 <tr>
-                    <td>Do website shit</td>
-                    <td>69</td>
-                    <td>2014-07-26</td>
+                    <td>'.$events['name'] .'</td>
+                    <td>'.$events['hours'].'</td>
+                    <td>'.$events['date'] .'</td>
                 </tr>
-                <tr>
-                    <td>Do more website shit</td>
-                    <td>13</td>
-                    <td>2014-07-27</td>
-                </tr>
-            </tbody>';
+';
+    }
     # End fancy code
     $page .= '
+            </tbody>
         </table>
     </div>
 </div>
 ';
-Page::write($page);
+    Page::write($page);
 }
 ?>
