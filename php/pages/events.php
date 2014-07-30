2662c0a97b18dcd9a7a400da63296a7397c41c5b
killmous <wcaldwell.email@gmail.com>
2014-07-29 23:35:32 -0500

    Edit merge fix events.php  Changes to be committed: 	modified:   php/pages/events.php

--- php/pages/events.php
+++ php/pages/events.php
@@ -7,8 +7,9 @@ function get_events() {
     <thead>
         <th>Event</th>
         <th>Possible hours</th>
-        <th>Date</th>
-    </thread>
+        <th>Date</th>' . (Util::getUser($_SESSION['bvid'])['rank'] >= 1 ?
+        '<th><button id="admin" class="btn btn-primary btn-sm">Admin<span class="glyphicon glyphicon-pencil"></span></button></th>':'') .
+    '</thead>
     <tbody>';
 
     foreach($database->select('events', '*') as $i) {
@@ -22,62 +23,109 @@ function get_events() {
     }
     if(Util::getUser($_SESSION['bvid'])['rank'] >= 1) {
         $page .= '
-        <tr>
+        <tr id="new-event-row">
             <td><input id="name-box" class="form-control" placeholder="Name" required="" autofocus></td>
             <td><input id="hour-box" class="form-control" placeholder="Possible Hour(s)" required=""></td>
             <td><input id="date-box" class="form-control" placeholder="Date" required=""></td>
             <td><button id="add-event" class="btn btn-primary btn-sm">Submit</button></td>
         </tr>
         <script>
-            $("#add-event").on("click", function(e) {
-                e.preventDefault();
-                $.ajax({
-                    type: "post",
-                    url: "/php/main.php",
-                    data: {
-                        util: "add_event",
-                        attr: {
-                            name: $("#name-box").val(),
-                            hours: $("#hour-box").val(),
-                            date: $("#date-box").val()
-                        }
-                    }
-                }).done(function(data) {
-                    $.ajax({
-                        type: "post",
-                        url: "/php/main.php",
-                        data: {
-                            page: "events"
-                        }
-                    }).done(function(data) {
-                        $("#main").html(data);
-                    });
-                });
+initAdminCtrls = function() {
+    $(".join-event").addClass("remove-event");
+    $(".join-event").removeClass("join-event");
+    $(".remove-event").text("Remove");
+    $("#new-event-row").show();
+    $("#add-event").on("click", function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "post",
+            url: "/php/main.php",
+            data: {
+                util: "add_event",
+                attr: {
+                    name: $("#name-box").val(),
+                    hours: $("#hour-box").val(),
+                    date: $("#date-box").val()
+                }
+            }
+        }).done(function(data) {
+            $.ajax({
+                type: "post",
+                url: "/php/main.php",
+                data: {
+                    page: "events"
+                }
+            }).done(function(data) {
+                $("#main").html(data);
             });
-            $(".remove-event").on("click", function(e) {
-                e.preventDefault();
-                eleid = $(this).attr("id");
-                $.ajax({
-                    type: "post",
-                    url: "/php/main.php",
-                    data: {
-                        util: "remove_event",
-                        attr: {
-                            id: eleid
-                        }
-                    }
-                }).done(function(d) {
-                    $.ajax({
-                        type: "post",
-                        url: "/php/main.php",
-                        data: {
-                            page: "events"
-                        }
-                    }).done(function(data) {
-                        $("#main").html(data);
-                    });
-                });
+        });
+    });
+    $(".remove-event").on("click", function(e) {
+        e.preventDefault();
+        eleid = $(this).attr("id");
+        $.ajax({
+            type: "post",
+            url: "/php/main.php",
+            data: {
+                util: "remove_event",
+                attr: {
+                    id: eleid
+                }
+            }
+        }).done(function(d) {
+            $.ajax({
+                type: "post",
+                url: "/php/main.php",
+                data: {
+                    page: "events"
+                }
+            }).done(function(data) {
+                $("#main").html(data);
             });
+        });
+    });
+};
+initMemberCtrls = function() {
+    $(".remove-event").addClass("join-event");
+    $(".remove-event").removeClass("remove-event");
+    $(".join-event").text("Join");
+    $("#new-event-row").hide();
+    $(".join-event").on("click", function(e) {
+        e.preventDefault();
+        eleid = $(this).attr("id");
+        $.ajax({
+            type: "post",
+            url: "/php/main.php",
+            data: {
+                util: "join_event",
+                attr: {
+                    id: eleid,
+                }
+            }
+        }).done(function(data) {
+            $.ajax({
+                type: "post",
+                url: "/php/main.php",
+                data: {
+                    page: "events"
+                }
+            }).done(function(data) {
+                $("#main").html(data);
+            });
+        });
+    });
+}
+initMemberCtrls();
+var mode = "member";
+$("#admin").on("click", function(e) {
+    if(mode == "member") {
+        initAdminCtrls();
+        mode = "admin";
+    } else {
+        initMemberCtrls();
+        mode = "member";
+    }
+});
         </script>';
     }
     $page .= '
