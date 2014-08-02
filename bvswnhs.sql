0ccbcf726329514675932708f0a78ec2e0afd31f
Wes Caldwell <wcaldwell.email@gmail.com>
2014-08-02 03:53:34 -0400

    Jacob made an oopsie
    Github is my favorite IDE

--- bvswnhs.sql
+++ bvswnhs.sql
@@ -45,7 +45,7 @@ CREATE TABLE IF NOT EXISTS `members` (
   `id` int(2) NOT NULL AUTO_INCREMENT,
   `name` varchar(30) NOT NULL,
   `email` varchar(30) NOT NULL,
-  `email` varchar(10) NOT NULL,
+  `phone` varchar(10) NOT NULL,
   `bvid` varchar(8) NOT NULL,
   `hours` int(11) NOT NULL,
   `rank` int(1) NOT NULL DEFAULT '0',
