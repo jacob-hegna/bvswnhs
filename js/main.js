fb1d97d31a4c294875ff5cb4be0ccaac06d440e0
Jacob Hegna <jacobhegna@gmail.com>
2014-08-07 15:30:23 -0400

    can't handle/10

--- js/main.js
+++ js/main.js
@@ -2,10 +2,6 @@ $(document).ready(function() {
     var url  = $.url();
     var p    = url.segment(1);
 
-    if(url.attr('host') == 'localhost' || url.attr('host') == '127.0.0.1') {
-        $('#wrap').css('background-color', '#ff0000');
-    }
-
     if(p == '') {
         history.pushState({}, "", "/home/");
         p = "home";
